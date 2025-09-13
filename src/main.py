ae7c6dfc33d2e490d44d8ee5ad7da94d1c8fd3e2
gilad-zimrin <gilad@zimrin.co.il>
2025-09-13 21:08:45 +0300

    fixed exception catching to routes

--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
-import logging
 from os import getenv
 
 import uvicorn
 
 from src.app_config import app
 from src.core.logger import logger
-
-
 
 
 def main():
@@ -16,9 +13,6 @@ def main():
         host='0.0.0.0',
         port=int(getenv("DAL_PORT", default=8000)),
     )
-    uvicorn_error = logging.getLogger("uvicorn.error")
-    uvicorn_error.disabled = True
-    uvicorn_error.propagate = False
 
 
 if __name__ == '__main__':
